5f1ae7ca42118303f2e6943b8e9e96795dc8e809
juliuskovac <julius@astratech.io>
2022-05-05 16:51:55 +0200

    JIRA-0091 formatted

--- .husky/index.js
+++ .husky/index.js
@@ -1,53 +1,60 @@
-const fs = require('fs');
-const { request } = require('http');
-const { release } = require('os');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const gitRootDir = __dirname + '/../';
-const messageFile = path.normalize(gitRootDir + '/' + process.argv[2]);
-const message = fs.readFileSync(messageFile, {encoding: 'utf-8'});
+const gitRootDir = __dirname + "/../";
+const messageFile = path.normalize(gitRootDir + "/" + process.argv[2]);
+const message = fs.readFileSync(messageFile, { encoding: "utf-8" });
 
 // allow exceptions
-if (message.startsWith('Pull request') || message.startsWith('Merge pull request')) {
-    process.exit(0);
+if (
+  message.startsWith("Pull request") ||
+  message.startsWith("Merge pull request")
+) {
+  process.exit(0);
 }
-if (message.startsWith('major release') || message.startsWith('minor release') || message.startsWith('patch release')) {
-    process.exit(0);
+if (
+  message.startsWith("major release") ||
+  message.startsWith("minor release") ||
+  message.startsWith("patch release")
+) {
+  process.exit(0);
 }
-if (message.startsWith('release')) {
-    process.exit(0);
+if (message.startsWith("release")) {
+  process.exit(0);
 }
 
-const parts = message.split(" ")
+const parts = message.split(" ");
 
 if (parts.length < 2) {
-    reportError( 'commit message needs to have at least two parts')
+  reportError("commit message needs to have at least two parts");
 }
-const issue = parts[0]
+const issue = parts[0];
 
 // https://stackoverflow.com/questions/19322669/regular-expression-for-a-jira-identifier
-const jiraFormat = /\d+-[A-Z]+(?!-?[a-zA-Z]{1,10})/g
+const jiraFormat = /\d+-[A-Z]+(?!-?[a-zA-Z]{1,10})/g;
 
-const issueReversed = reverse(issue)
-const valid = jiraFormat.test(issueReversed)
-if (!valid) { 
-    reportError('invalid JIRA issue')
+const issueReversed = reverse(issue);
+const valid = jiraFormat.test(issueReversed);
+if (!valid) {
+  reportError("invalid JIRA issue");
 }
 
 // message start with non whitespace character
 // [^\s] match a single character not present in the list
-const messageStart = parts[1]
-const messageFormat = /^[^\s].*/
-const validMessage = messageFormat.test(messageStart)
+const messageStart = parts[1];
+const messageFormat = /^[^\s].*/;
+const validMessage = messageFormat.test(messageStart);
 if (!validMessage) {
-    reportError('invalid commit description')
+  reportError("invalid commit description");
 }
 
 function reverse(s) {
-    return [...s].reverse().join("");
+  return [...s].reverse().join("");
 }
 
 function reportError(reason) {
-    console.log(`please use commit message in format "JIRA-nnnn commit description" (${reason})`)
-    process.exit(1);
+  console.log(
+    `please use commit message in format "JIRA-nnnn commit description" (${reason})`
+  );
+  process.exit(1);
 }
