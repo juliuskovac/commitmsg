27e6d4856849588acb11cb9d61dc47bfc911a91c
juliuskovac <julius@astratech.io>
2022-05-05 11:25:19 +0200

    JIRA-123 test commit

--- .husky/index.js
+++ .husky/index.js
@@ -2,15 +2,27 @@ const fs = require('fs');
 const path = require('path');
 
 const gitRootDir = __dirname + '/../';
-
-console.log(__dirname);
-
 const messageFile = path.normalize(gitRootDir + '/' + process.argv[2]);
-
 const message = fs.readFileSync(messageFile, {encoding: 'utf-8'});
 
 console.log('message', message);
 
-console.log('index in root')
-console.log(process.argv);
-//console.log(process.env.HUSKY_GIT_PARAMS.split(' ')[0])
+const issue = message.split(" ")[0]
+
+// https://stackoverflow.com/questions/19322669/regular-expression-for-a-jira-identifier
+var jira_matcher = /\d+-[A-Z]+(?!-?[a-zA-Z]{1,10})/g
+
+function reverse(s) {
+    return [...s].reverse().join("");
+}
+
+const issueReversed = reverse(issue)
+
+const valid = jira_matcher.test(issueReversed)
+
+if (valid) { 
+    console.log('valid JIRA issue format')
+} else {
+    console.log('invalid JIRA issue format')
+    process.exit(1);
+}
