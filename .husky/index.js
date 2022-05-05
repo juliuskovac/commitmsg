478102838749c39edad6f57f5e8c0c9302c8d556
juliuskovac <julius@astratech.io>
2022-05-05 10:55:16 +0200

    test commit

--- .husky/index.js
+++ .husky/index.js
@@ -5,6 +5,12 @@ const gitRootDir = __dirname + '/../';
 
 console.log(__dirname);
 
+const messageFile = path.normalize(gitRootDir + '/' + process.argv[2]);
+
+const message = fs.readFileSync(messageFile, {encoding: 'utf-8'});
+
+console.log('message', message);
+
 console.log('index in root')
 console.log(process.argv);
 //console.log(process.env.HUSKY_GIT_PARAMS.split(' ')[0])
