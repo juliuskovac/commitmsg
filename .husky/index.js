80d4307543efb6ec3c0771e23d2a1b50aafa12df
juliuskovac <julius@astratech.io>
2022-05-05 10:53:35 +0200

    test commit

--- .husky/index.js
+++ .husky/index.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const gitRootDir = __dirname + '/../';
+
+console.log(__dirname);
+
 console.log('index in root')
 console.log(process.argv);
 //console.log(process.env.HUSKY_GIT_PARAMS.split(' ')[0])
